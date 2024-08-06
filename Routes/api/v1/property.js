4a5f104b7e964516da858c3b6089970be2954f2e
shivam30072 <shivam30072@gmail.com>
2024-08-06 23:00:19 +0530

    pagination added in all properties api

--- Routes/api/v1/property.js
+++ Routes/api/v1/property.js
@@ -8,6 +8,6 @@ const {
 // baseUrl here is `/api/v1/property`
 const router = require("express").Router();
 
-router.get("/getProperties", getProperties);
+router.post("/getProperties", getProperties); // post route just because getting pagenumber and pagesize in body for pagination
 router.post("/uploadProperties", uploadProperties);
 module.exports = router;
--- controllers/property/controller.js
+++ controllers/property/controller.js
@@ -3,8 +3,19 @@ const property = require("../../models/property");
 
 const getProperties = async (req, res) => {
   try {
-    const response = await property.find({});
-    return res.status(200).json({ success: true, data: response });
+    const pageNumber = parseInt(req.body.pageNumber) || 1;
+    const pageSize = parseInt(req.body.pageSize) || 5;
+
+    const skip = (pageNumber - 1) * pageSize;
+
+    const totalDocuments = await property.countDocuments({});
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+
+    const response = await property.find({}).skip(skip).limit(pageSize);
+    return res.status(200).json({
+      success: true,
+      data: { response, pageNumber, pageSize, totalPages },
+    });
   } catch (error) {
     console.log(error);
     throw error;
